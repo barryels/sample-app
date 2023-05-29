532aa5f23b87adf1510a8da6f0f9047860a463b2
Barry Els <barryels@gmail.com>
2023-05-29 13:07:05 +0100

    add ability to go back home from login

--- cypress/e2e/example.cy.ts
+++ cypress/e2e/example.cy.ts
@@ -4,9 +4,12 @@ describe("Testing Cypress setup and config", () => {
     cy.findByRole("heading", { name: "Foodie" }).should("be.visible");
   });
 
-  it("User can view the login form", () => {
+  it("User can view the login form and navigate back to home", () => {
     cy.visit("/");
     cy.findByRole("link", { name: "Login" }).click();
     cy.findByRole("heading", { name: "Login" }).should("be.visible");
+
+    cy.findByRole("link", { name: "Home" }).click();
+    cy.findByRole("heading", { name: "Foodie" }).should("be.visible");
   });
 });
--- src/screens/Login/index.tsx
+++ src/screens/Login/index.tsx
@@ -1,7 +1,11 @@
+import { Button } from "@mui/material";
+
 export default function Login() {
   return (
     <>
       <h1>Login</h1>
+
+      <Button href="/">Home</Button>
     </>
   );
 }
